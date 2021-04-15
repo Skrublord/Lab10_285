5844a983df5392ea78bb8ecff19dc9a78da14428
Nathan <nlipski13@gmail.com>
2021-04-14 21:08:33 -0400

    Edit testSelection.java Pos/Neg

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -20,36 +20,63 @@ public class testSelectionSort {
 
     public testSelectionSort() {
     }
-    
-    public void testPositive(){
+
+		public void testPositive(){
         int[] arr = new int[5];
         arr[0] = 8;
         arr[1] = 9;
         arr[2] = 7;
         arr[3] = 10;
         arr[4] = 2;
-        
+
         int[] Sortedarr = new int[5];
         Sortedarr[0] = 2;
         Sortedarr[1] = 7;
         Sortedarr[2] = 8;
         Sortedarr[3] = 9;
         Sortedarr[4] = 10;
-        
-        /** add tests to check for this unit test **/
+
+        int[] TestArr = new int[5];
+        SelectionSort temp1 = new SelectionSort();
+		TestArr = temp1.basicSelectionSort(arr);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(TestArr[i]);
+			assertEquals("testPositive incorrect value", Sortedarr[i], TestArr[i]);
+		}
+		System.out.println();
     }
-    
+
     public void testNegative(){
-        
-        /** Test data contains negative values only **/
+    	int[] arr = new int[5];
+        arr[0] = -8;
+        arr[1] = -9;
+        arr[2] = -7;
+        arr[3] = -10;
+        arr[4] = -2;
+
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = -10;
+        Sortedarr[1] = -9;
+        Sortedarr[2] = -8;
+        Sortedarr[3] = -7;
+        Sortedarr[4] = -2;
+
+        int[] TestArr = new int[5];
+        SelectionSort temp1 = new SelectionSort();
+		TestArr = temp1.basicSelectionSort(arr);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(TestArr[i]);
+			assertEquals("testNegative incorrect value", Sortedarr[i], TestArr[i]);
+		}
+		System.out.println();
     }
-    
+
     public void testMixed(){
-        
+
         /** Test data contains with both positive, negative and zeros **/
     }
-    
+
     public void testDuplicates(){
-        
+
         /** Test data contains duplicates **/
     }
